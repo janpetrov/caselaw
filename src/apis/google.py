ca61b1607f22502b6a730aacb5413f9a2d517c1e
Jan Petrov <janpetrov@icloud.com>
2025-03-27 23:13:56 +0100

    update google.py

--- src/apis/google.py
+++ src/apis/google.py
@@ -3,7 +3,7 @@ from google import genai
 from google.genai import types
 
 
-def gemini_infer(text):
+def gemini_infer(text, top_k=40, top_p=0.95, temperature=1):
     client = genai.Client(
         api_key=os.environ.get("GEMINI_API_KEY"),
     )
@@ -18,9 +18,9 @@ def gemini_infer(text):
         ),
     ]
     generate_content_config = types.GenerateContentConfig(
-        temperature=1,
-        top_p=0.95,
-        top_k=40,
+        temperature=temperature,
+        top_p=top_p,
+        top_k=top_k,
         max_output_tokens=8192,
         response_mime_type="text/plain",
     )
